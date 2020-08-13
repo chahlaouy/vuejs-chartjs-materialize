2122af9cd8cc54c7741bfb775ef512a18fcc0959
chahlaouy <chahlaouy1991@gmail.com>
2020-08-13 13:52:02 +0200

    chartjs

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -27,12 +27,56 @@
                 </div>
             </div>
 
+
+            <!-- line Chart -->
+            <div class="row">
+                <div class="col s4"></div>
+                <div class="col s4">
+                    <line-chart
+                        :chartData="dataCollection"
+                    ></line-chart>
+
+                </div>
+                <div class="col s4"></div>
+            </div>
         </form>
     </div>
 </template>
 
 <script>
-export default {
+import LineChart from './chartjsLine'
+
+export default { 
+    data(){
+        return {
+            dataCollection: null,
+            loaded: false
+        }
+    },
+    methods: {
+        fillData(){
+            this.dataCollection = {
+                labels: [1,2,3,4,5,6,7,8],
+                datasets: [
+                    {
+                        label: 'Temp (C)',
+                        backgroundColor: 'blue',
+                        borderWidth: 1,
+                        borderColor: 'black',
+                        pointBackgroundColor: 'orange',
+                        pointBorderColor: 'blue',
+                        data: [1,2,3,4,5,6,7,8]
+                    },
+                ]
+            }
+        }
+    },
+    mounted(){
+        this.fillData()
+    },
+    components: {
+        LineChart
+    }
     
 }
 </script>
