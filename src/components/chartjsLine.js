4a8c2cf9d7e3a9787d47c1782bc4499c869bcdc6
chahlaouy <chahlaouy1991@gmail.com>
2020-08-13 14:04:15 +0200

    chartjs

--- src/components/chartjsLine.js
+++ src/components/chartjsLine.js
@@ -1,11 +1,11 @@
 
-import { Line, mixins } from 'vue-chartjs'
+import { Pie, mixins } from 'vue-chartjs'
 
 const { reactiveProp } = mixins
 
 export default {
 
-    extends: Line,
+    extends: Pie,
     mixins: [reactiveProp],
     props: {
         chartData: {
